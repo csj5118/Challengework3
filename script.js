c8fdd43e0dd0206bf780f7b6ecdd2ebaa7f05cce
CJ <csh.joo77@gmail.com>
2024-02-10 21:05:00 -0500

    added variables and function for password length check

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var Lower = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var Upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var num = '0123456789';
+var Special = '!"#$%&()*+,-./:;<=>?@[\]^_{|}~';
+var passwordLength;
+var uppercase;
+var lowercase;
+var specialchar;
+
+
+
+
+function PasswordLength() {
+  PasswordLength = prompt('Please tell us how long you wish your password to be. Must be greater than 8 and less than 128');
+  return parseInt(PasswordLengthString, 10);
+}
 
 function generatePassword() {
   var password = "";
-
   // add code to change passqword variable.
 
   // window.prompt
